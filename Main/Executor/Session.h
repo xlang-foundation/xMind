2f21e4a18a6557331c37d4daddf281a38ff37bb7
Galaxy <81903380+OliviaGalaxy3DVision@users.noreply.github.com>
2024-09-25 03:05:43 -0700

    fix bugs

--- Main/Executor/Session.h
+++ Main/Executor/Session.h
@@ -1,3 +1,22 @@
+﻿/*
+Copyright (C) 2024 The XLang Foundation
+
+This file is part of the xMind Project.
+
+xMind is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+xMind is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with xMind. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 #include "singleton.h"
 #include <string>
